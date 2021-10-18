dcf034a4fa133343c03fbc0152b91255c8328367
Manuel <manuel.guth@gmx.net>
2021-10-18 15:09:59 +0200

    initial commit of client

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var stopwatchHelper = new StopwatchClientHelper();
+            stopwatchHelper.Start();
+
+            Thread.Sleep(5000);
+
+            var isRunning = await stopwatchHelper.IsRunning();
+
+            Thread.Sleep(3000);
+
+            var time = await stopwatchHelper.GetTimeInMS();
+
+            Thread.Sleep(2000);
+
+            stopwatchHelper.Stop();
+
+            Console.WriteLine("Press any Key to exit!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcClient/StopwatchClientHelper.cs
@@ -0,0 +1,64 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    public class StopwatchClientHelper
+    {
+        private GrpcChannel _Channel;
+        private Stopwatch.StopwatchClient _StopwatchClient;
+
+        public StopwatchClientHelper()
+        {
+            _Channel = GrpcChannel.ForAddress("https://localhost:5001"); // TODO: einlesbar machen von appsetting oder so
+            _StopwatchClient = new Stopwatch.StopwatchClient(_Channel);
+        }
+        /// <summary>
+        /// Starts/Continues the time on the server
+        /// </summary>
+        public async void Start()
+        {
+            var start = new StartTime();
+            Console.WriteLine("Start timing."); // TODO: Logging optional
+            await _StopwatchClient.StartAsync(start);
+        }
+
+        /// <summary>
+        /// Stops/Pauses the time on the server
+        /// </summary>
+        public async void Stop()
+        {
+            var stop = new StopTime();
+            Console.WriteLine("Stoping...");
+            await _StopwatchClient.StopAsync(stop);
+        }
+
+        /// <summary>
+        /// Gets the current State of the server
+        /// </summary>
+        /// <returns>
+        /// true - if time is running
+        /// false - if time stopped
+        /// </returns>
+        public async Task<bool> IsRunning()
+        {
+            var status = new GetStatus();
+            var response = await _StopwatchClient.GetCurrentStatusAsync(status);
+            Console.WriteLine($"Running: {response.IsRunning}");
+            return response.IsRunning;
+        }
+
+        /// <summary>
+        /// Gets the current measured time from the server 
+        /// </summary>
+        /// <returns>time in milliseconds</returns>
+        public async Task<long> GetTimeInMS()
+        {
+            var timeRequest = new GetTime();
+            var response = await _StopwatchClient.GetElapsedTimeMillisecondsAsync(timeRequest);
+            Console.WriteLine($"Current Runtime: {response.Milliseconds}");
+            return response.Milliseconds;
+        }
+    }
+}
