585b835873f39922b63b51309cf1ea873363d090
Manuel <manuel.guth@gmx.net>
2021-10-19 13:18:51 +0200

    add UI Client

---
+++ StopWatchService/GrpcUiClient/DelegateCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GrpcClientUi
+{
+    /// <summary>
+    /// Idea for class implementation taken from https://www.wpftutorial.net/DelegateCommand.html
+    /// </summary>
+    class DelegateCommand : ICommand
+    {
+        readonly Action<object> execute;
+        readonly Predicate<object> canExecute;
+
+        public DelegateCommand(Predicate<object> canExecute, Action<object> execute) =>
+            (this.canExecute, this.execute) = (canExecute, execute);
+
+        public DelegateCommand(Action<object> execute) : this(null, execute) { }
+       
+        public event EventHandler CanExecuteChanged;
+
+        public void RaisCanExecuteChanged() => this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public bool CanExecute(object parameter) => this.canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => this.execute?.Invoke(parameter);
+    }
+}
---
+++ StopWatchService/GrpcUiClient/MainWindowViewModel.cs
@@ -0,0 +1,114 @@
+﻿using GrpcClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Input;
+
+namespace GrpcClientUi
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        StopwatchClientHelper stopwatchClientHelper;
+        private Timer refreshTimer;
+        public MainWindowViewModel()
+        {
+            Status = "";
+            Time = "";
+
+            stopwatchClientHelper = new StopwatchClientHelper();
+
+            StopCommand = new DelegateCommand(
+                o => true,
+                o => { StopButtonKlick(); }
+                );
+
+            StartCommand = new DelegateCommand(
+                o => true,
+                o => { StartButtonKlick(); }
+                );
+
+            InitRefreshTimer();
+        }
+
+        string status;
+        public string Status
+        {
+            get => status;
+            set
+            {
+                if (status != value)
+                {
+                    status = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        string time;
+        public string Time
+        {
+            get => time;
+            set
+            {
+                if (time != value)
+                {
+                    time = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public DelegateCommand StopCommand { get; set; }
+        public DelegateCommand StartCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void InitRefreshTimer()
+        {
+            // Create a timer with a 200 milliseconds interval.
+            refreshTimer = new Timer(200);
+            // Hook up the Elapsed event for the timer. 
+            refreshTimer.Elapsed += RefreshTimerEvent;
+            refreshTimer.AutoReset = true;
+            refreshTimer.Enabled = true;
+        }
+
+        private async void RefreshTimerEvent(Object source, ElapsedEventArgs e)
+        {
+            var isRunning = await stopwatchClientHelper.IsRunning();
+            var time = await stopwatchClientHelper.GetTimeInMS();
+
+            Time = FormatTime(time);
+            Status = isRunning ? "Running" : "Stopped";
+        }
+
+        private string FormatTime(long timeInMs)
+        {
+            TimeSpan ts = TimeSpan.FromMilliseconds(timeInMs);
+            return ts.ToString(@"hh\:mm\:ss");
+        }
+
+        public void StartButtonKlick()
+        {
+            stopwatchClientHelper.Start();
+        }
+
+        public void StopButtonKlick()
+        {
+            stopwatchClientHelper.Stop();
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (!string.IsNullOrEmpty(propertyName))
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
